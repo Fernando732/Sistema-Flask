599ee6bda4290b419a5dfb809ba112a114d768f8
Fernando <fernandomartinezsa9@gmail.com>
2023-01-24 20:18:28 -0600

    configuraciones

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\wuero\OneDrive\Documentos\ProyectoFlask\venv
+set VIRTUAL_ENV=D:\Proyecto_Base_De_Datos\Sistema-Flask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
